d5235b56a28679a6487c33850099825469453954
Igor Strapko <igor@goodpraxis.coop>
2020-05-14 16:20:55 +0100

    Add theme to conf file

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ PATH = 'content'
 TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = 'en'
-
+THEME = 'basic'
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
